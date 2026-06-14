cd427829d035f2c147f7b75a8eef36215373506c
Nabil Fayez <108990650+nabil-fayez@users.noreply.github.com>
2026-06-14 11:55:22 +0300

    set routes folder structure

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\Auth\ShowLoginController;
 use Illuminate\Support\Facades\Route;
 
+// Public Routes
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Admin domain routes
 require __DIR__ . '/admin.php';
+
 // Tenant staff domain routes
-
-
+require __DIR__ . '/tenant.php';
 
 // User domain routes
+require __DIR__ . './user.php'
